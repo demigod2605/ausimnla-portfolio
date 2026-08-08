3184e6214812e2bdab25e53fc3d6eb4cf013c737
ausi <shaneaustinmmanila@gmail.com>
2026-08-08 13:14:29 +0800

    Update certificate title and add source image

--- lib/certificates.ts
+++ lib/certificates.ts
@@ -3,7 +3,7 @@ import type { Certificate } from "@/lib/types";
 export const certificates: Certificate[] = [
   {
     id: "cert-01",
-    title: "Certificate of Completion",
+    title: "Certificate of Participation",
     issuer: "INTEGRATED SOCIETY OF INFORMATION TECHNOLOGY ENTHUSIASTS (iSITE) INC.",
     year: "November 2023",
     image: "/certificate.png",
