acca8811f38dd9810d9e2736127a7fed7c768a63
ausi <shaneaustinmmanila@gmail.com>
2026-08-08 13:09:45 +0800

    Set certificate issuer and date

--- lib/certificates.ts
+++ lib/certificates.ts
@@ -4,8 +4,8 @@ export const certificates: Certificate[] = [
   {
     id: "cert-01",
     title: "Certificate of Completion",
-    issuer: "Issuer name",
-    year: "2026",
+    issuer: "INTEGRATED SOCIETY OF INFORMATION TECHNOLOGY ENTHUSIASTS (iSITE) INC.",
+    year: "November 2023",
     image: "/certificate.png",
   },
 ];
